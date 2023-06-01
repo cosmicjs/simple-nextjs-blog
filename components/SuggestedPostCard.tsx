7fdd9f9f18a84d9ade6816f79584fb107b1731aa
Karl Koch <karl@kejk.tech>
2023-06-01 21:07:24 +0100

    hotfix: image cropping on suggested posts

--- components/SuggestedPostCard.tsx
+++ components/SuggestedPostCard.tsx
@@ -9,13 +9,7 @@ export default function PostCard({ post }: { post: Post }) {
     <div>
       {post.metadata.hero?.imgix_url && (
         <Link href={`/posts/${post.slug}`}>
-          <Image
-            className='mb-5 rounded-xl bg-no-repeat object-cover object-center transition-transform duration-200 ease-out hover:scale-[1.02]'
-            src={`${post.metadata.hero?.imgix_url}?w=1400&auto=format`}
-            width={1400}
-            height={340}
-            alt={post.title}
-          />
+          <Image className='mb-5 h-[240px] rounded-xl bg-no-repeat object-cover object-center transition-transform duration-200 ease-out hover:scale-[1.02]' src={`${post.metadata.hero?.imgix_url}?w=1400&auto=format`} width={1400} height={240} alt={post.title} />
         </Link>
       )}
       <h2 className='pb-3 text-xl font-semibold text-zinc-800 dark:text-zinc-200'>
