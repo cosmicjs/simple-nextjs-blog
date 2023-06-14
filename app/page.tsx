7fd7df379664ae833781004ec40b20c35418f7b7
Tony Spiro <tspiro@tonyspiro.com>
2023-06-13 20:07:26 -0700

    add: revalidate

--- app/page.tsx
+++ app/page.tsx
@@ -19,3 +19,4 @@ export default async function Page(): Promise<JSX.Element> {
     </main>
   );
 }
+export const revalidate = 60
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -70,3 +70,4 @@ export default async ({ params }: { params: { slug: string } }) => {
     </>
   );
 };
+export const revalidate = 60
