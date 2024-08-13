6c5e52d6df9d78c2154a39a90c1fc75f23311beb
Tony Spiro <tspiro@tonyspiro.com>
2024-08-13 11:46:59 -0700

    edit: test image optimization

--- components/SuggestedPostCard.tsx
+++ components/SuggestedPostCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 import helpers from '../helpers';
 import { Post } from '../lib/types';
 
--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const nextConfig = {
   experimental: {
     ppr: 'incremental',
   },
-  images: {
-    domains: ['imgix.cosmicjs.com'],
-    formats: ['image/avif', 'image/webp'],
-  },
 };
 
 export default nextConfig;
