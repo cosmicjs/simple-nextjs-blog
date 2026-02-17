2e52bf6adc36af105382728635039331e3782fd6
Tony Spiro <tspiro@tonyspiro.com>
2026-02-16 22:12:44 -0800

    Add dynamic site tag display back to SiteLogo component

--- components/logos/SiteLogo.tsx
+++ components/logos/SiteLogo.tsx
@@ -9,8 +9,8 @@ export default function SiteLogo({
   siteData: GlobalData;
 }): React.ReactElement {
   return (
-    <div className="mx-auto flex max-w-3xl items-center p-4 lg:px-0">
-      {/* Changed: Reverted to dynamic site title from Cosmic CMS instead of hardcoded text */}
+    <div className="mx-auto flex max-w-3xl items-center justify-between p-4 lg:px-0">
+      {/* Changed: Added site_tag back alongside the site title */}
       <h1 className="flex space-x-2">
         <OBMLogo className="h-8 w-8" />
         <Link
@@ -20,6 +20,12 @@ export default function SiteLogo({
           {siteData?.metadata?.site_title ?? 'Our Blue Marble'}
         </Link>
       </h1>
+      {/* Changed: Added site_tag display from Cosmic CMS globals */}
+      {siteData?.metadata?.site_tag && (
+        <p className="hidden text-zinc-600 dark:text-zinc-400 md:block">
+          {siteData.metadata.site_tag}
+        </p>
+      )}
     </div>
   );
 }
