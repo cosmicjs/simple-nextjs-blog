706880e7c52048db1bbd594f50d116837b991080
Tony Spiro <tspiro@tonyspiro.com>
2026-02-16 22:07:49 -0800

    Fix site logo icon and title text styling to match original design

--- components/logos/SiteLogo.tsx
+++ components/logos/SiteLogo.tsx
@@ -11,13 +11,13 @@ export default function SiteLogo({
   return (
     <div className="mx-auto flex max-w-3xl items-center space-x-4 p-4 lg:px-0">
       <Link href="/" className="flex items-center space-x-3">
-        <OBMLogo className="w-8 md:w-10" />
-        <div>
-          <span className="text-lg font-bold text-zinc-900 md:text-2xl dark:text-zinc-100">
+        <OBMLogo className="w-10 md:w-12" />
+        <div className="flex items-baseline space-x-3">
+          <span className="text-xl font-extrabold tracking-tight text-zinc-900 md:text-3xl dark:text-zinc-100">
             {siteData?.metadata?.site_title}
           </span>
-          <span className="text-zinc-500 md:text-lg dark:text-zinc-400">
-            &nbsp;&nbsp;{siteData?.metadata?.site_tag}
+          <span className="text-sm text-zinc-500 md:text-lg dark:text-zinc-400">
+            {siteData?.metadata?.site_tag}
           </span>
         </div>
       </Link>
